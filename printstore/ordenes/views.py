1a379817c4e0554dd39cfe958d53f0a3e6a3f070
unknown <mirkorojas345@gmail.com>
2023-12-16 15:14:47 -0500

    Registro de ordenes con modal

--- printstore/ordenes/views.py
+++ printstore/ordenes/views.py
@@ -50,20 +50,33 @@ def Ordenes(request):
 
 def Create_orden(request):
     if request.method == 'POST':
-        data = json.loads(request.body)
+        response = json.loads(request.body)
+        print(response)
         defaultDatas = [] #DetalleOrdem
         uniqueDatas = [] #ProductoEspecificacion
-        for id in data['array_id_productos']:
+        objOrden = {'nombre_cliente': "", 'apellido_cliente': "", 'dni_cliente':0,'fecha_entrega':None}
+        for id in response['array_id_productos']:
             print(id)
-            for data in data['data']:
+            for data in response['data']:
                 objDefaultData = {'id_producto':0,'nombre_attr':"",'valor':''}
                 objUniqueAttr = {'id_producto':0,'id_especificacion':0,'valor_seleccionado':"",'tipo_dato':""}
-                print(data)
+                objClienteData = {'attr':"",'valor':""}
                 if str(data['id']).split('_')[0] == 'id' and int(str(data['id']).split('_')[len(str(data['id']).split('_'))-1]) == id:
                     objDefaultData['id_producto']=int(str(data['id']).split('_')[len(str(data['id']).split('_'))-1])
                     objDefaultData['nombre_attr']=str(str(data['id']).split('_')[1])
                     objDefaultData['valor']=float(data['value']) if (data['type']=="number") else data['value']
                     defaultDatas.append(objDefaultData)
+                elif str(data['id']).split('_')[len(str(data['id']).split('_'))-1].lower() == 'cliente':
+                    objClienteData['attr'] = str(data['id']).split('_')[0]
+                    objClienteData['valor'] = int(data['value']) if (data['type']=="number") else data['value']
+
+                    if objClienteData['attr'] == "nombre":
+                        objOrden['nombre_cliente'] = objClienteData['valor']
+                    elif objClienteData['attr'] == "apellido":
+                        objOrden['apellido_cliente'] = objClienteData['valor']
+                    else :
+                        objOrden['dni_cliente'] = objClienteData['valor']
+
                 else:
                     objUniqueAttr['id_producto']=int(str(data['id']).split('_')[len(str(data['id']).split('_'))-2])
                     objUniqueAttr['id_especificacion']=int(str(data['id']).split('_')[len(str(data['id']).split('_'))-1])
@@ -74,10 +87,31 @@ def Create_orden(request):
                     else:
                         objUniqueAttr['valor_seleccionado']=str(data['value'])
                     uniqueDatas.append(objUniqueAttr)        
-        print("Default data".center(50,'-'))
-        print(defaultDatas)
-        print("Unique data".center(50,'-'))
-        print(uniqueDatas)
+        user = request.user
+        objOrden['user'] = user
+        ordenRegistrada = Orden.objects.create(**objOrden) #El ** es para desempaquetado de diccionario
+
+        for array_id in response['array_id_productos']:
+            objDetalleOrden={'fk_id_orden':ordenRegistrada,'cantidad_ejemplares':0,'colores_impresion':"",'ancho':0,'largo':0}
+            objDetalleOrden['cantidad_ejemplares']=int(list(e for e in defaultDatas if (e['nombre_attr']  == "cantidad" and int(e['id_producto'])==array_id))[0]['valor'])
+            objDetalleOrden['colores_impresion']=str(list(e for e in defaultDatas if (e['nombre_attr']  == "colores" and int(e['id_producto'])==array_id))[0]['valor'])
+            objDetalleOrden['largo']=float(list(e for e in defaultDatas if (e['nombre_attr']  == "largo" and int(e['id_producto'])==array_id))[0]['valor'])
+            objDetalleOrden['ancho']=float(list(e for e in defaultDatas if (e['nombre_attr']  == "ancho" and int(e['id_producto'])==array_id))[0]['valor'])
+            print(objDetalleOrden)
+            detalleOrdenRegistrado = DetalleOrden.objects.create(**objDetalleOrden)
+            
+            for objEsp in uniqueDatas:
+                objProductoEspecificacion = ProductoEspecificacion.objects.filter(fk_id_producto=array_id,fk_id_especificacion=int(objEsp['id_especificacion'])).first()
+                objDetalleOrdenProductoEspecificacion = {'fk_id_detalle_orden':detalleOrdenRegistrado,'fk_id_producto_especificacion':objProductoEspecificacion,'valor_seleccionado':objEsp['valor_seleccionado']}
+                DetalleOrdenProductoEspecificacion.objects.create(**objDetalleOrdenProductoEspecificacion)
+            print(detalleOrdenRegistrado)
+            
+        # print("Cliente data".center(50,'-'))
+        # print(dataCliente)
+        # print("Default data".center(50,'-'))
+        # print(defaultDatas)
+        # print("Unique data".center(50,'-'))
+        # print(uniqueDatas)
         return HttpResponse(json.dumps(data))
     else:
         return HttpResponse('Invalid request method')
